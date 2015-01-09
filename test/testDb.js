bc508f80e707cd94e1d51974793fd5c292106a19
KingTree <ahwangshu@qq.com>
2015-01-09 14:58:30 +0800

    增加404页面和过滤器

--- test/testDb.js
+++ test/testDb.js
@@ -20,27 +20,38 @@ var log4js = require('./../app').logger();
 var mongodb = require('../util/mongoJoin').db;
 /* GET home page. */
 router.get('/', function(req, res) {
-
+    req.session.user={name:'wangshu',pass:'admin1234'};
     var news = mongodb.collection('news');
-    /* news.insert({
-     "title" : "新闻标题7",
+    /*news.insert({
+     "title" : "新闻标题8",
      "contert" : "这是新闻内容",
      "pulisher" : "发布者",
-     "publishTime" : new Date().toLocaleString()
+     "publishTime" : new Date()
      });*/
     /*news.count(function(err,count){
         console.log(count);
     });*/
+    news.find({publishTime : { "$gte": new Date('2015-01-09'),"$lt":new Date('2015-01-11')}},function(err,data){
+       // console.log(data);
 
-    news.find({},function(err,data){
         data.each(function(err,doc){
-            console.log(doc._id);
-            console.log(doc.title);
+            /*console.log(doc._id);
+            console.log(doc.title);*/
+            console.log(doc);
         });
-
     });
 
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express',userName:'haha' });
+});
+
+router.get('/add',function(req,res){
+    var userName = req.session.user.name;
+    res.render('index',{ title: 'Express',userName:userName});
+});
+
+router.get('/rms',function(req,res){
+    req.session.destroy();
+    res.render('index',{ title: 'Express',userName:'aaaa'});
 });
 
 module.exports = router;
